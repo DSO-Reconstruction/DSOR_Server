5258dc53d1b969ec2f0c73a7a570b2e40ba0e093
DarkZ0ne <nicolas.contrent@gmail.com>
2024-10-18 00:53:19 +0200

    Update DSO_Server_Hexdump.js

--- DSO_Server_Hexdump.js
+++ DSO_Server_Hexdump.js
@@ -6,26 +6,26 @@ const PORT = 2190;
 const HOST = '0.0.0.0';
 
 server.on('message', (msg, rinfo) => {
-    console.log(`Message reçu de ${rinfo.address}:${rinfo.port}`);
+    console.log(`Message from ${rinfo.address}:${rinfo.port}`);
 
-    console.log(`Contenu binaire (hexdump) : ${msg.toString('hex')}`);
+    console.log(`Binary content (hexdump) : ${msg.toString('hex')}`);
 
     let header = msg.slice(0, 4);
     let headerHex = header.toString('hex');
-    console.log(`En-tête hexadécimal : ${headerHex}`);
+    console.log(`Hexa header : ${headerHex}`);
 
     let number = msg.readUInt32BE(12);
-    console.log(`Nombre lu à partir de l'offset 12 : ${number}`);
+    console.log(`Number read at the offset 12 : ${number}`);
 });
 
 server.on('error', (err) => {
-    console.error(`Erreur : ${err.stack}`);
+    console.error(`Error : ${err.stack}`);
     server.close();
 });
 
 server.on('listening', () => {
     const address = server.address();
-    console.log(`Serveur UDP en écoute sur ${address.address}:${address.port}`);
+    console.log(`Serveur UDP listening on : ${address.address}:${address.port}`);
 });
 
 server.bind(PORT, HOST);
